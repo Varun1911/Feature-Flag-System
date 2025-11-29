2ad7ddcaddd1544d1d0cc27180456b3d158c7638
Varun1911 <varunt298@gmail.com>
2025-11-29 22:32:53 +0530

    added flag model and flag types

---
+++ backend/src/core/models/flag.model.ts
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+// Variant Schema
+const VariantSchema = new mongoose.Schema({
+  key: { type: String, required: true },
+  value: { type: mongoose.Schema.Types.Mixed, required: true }
+});
+
+// Condition Schema
+const ConditionSchema = new mongoose.Schema({
+  attribute: { type: String, required: true },
+  operator: { type: String, required: true },
+  value: { type: mongoose.Schema.Types.Mixed, required: true }
+});
+
+// Rollout Schema
+const RolloutSchema = new mongoose.Schema({
+  variant: { type: String, required: true },
+  percentage: { type: Number, required: true }
+});
+
+// Rule Schema (conditions ANDed → rollout applied)
+const RuleSchema = new mongoose.Schema({
+  conditions: [ConditionSchema], 
+  rollout: [RolloutSchema]
+});
+
+// Environment Schema (rules ORed)
+const EnvironmentConfigSchema = new mongoose.Schema({
+  isEnabled: { type: Boolean, default: false },
+  defaultVariant: { type: String, required: true },
+  rules: [RuleSchema]
+});
+
+// Full Flag Schema
+const FlagSchema = new mongoose.Schema(
+  {
+    key: { type: String, required: true, unique: true },
+    description: { type: String },
+    variants: [VariantSchema],
+
+    environments: {
+      dev: EnvironmentConfigSchema,
+      prod: EnvironmentConfigSchema
+    },
+
+    tags: [String],
+    createdBy: { type: String },
+    updatedBy: { type: String }
+  },
+  { timestamps: true }
+);
+
+export const FlagModel = mongoose.model("FeatureFlag", FlagSchema);
---
+++ backend/src/core/types/flag.ts
@@ -0,0 +1,26 @@
+export type FlagCondition = {
+  attribute: string;   
+  operator: "equals" | "not_equals" | "in" | "not_in" | "contains";
+  value: any;
+};
+
+export type RolloutRule = {
+  variant: string;
+  percentage: number; // 0–100
+};
+
+export type FlagRule = {
+  conditions: FlagCondition[];  // AND inside rule
+  rollout: RolloutRule[];       // applied if rule matches
+};
+
+export type EnvironmentConfig = {
+  isEnabled: boolean;
+  defaultVariant: string;
+  rules: FlagRule[];            // OR across rules
+};
+
+export type FlagVariant = {
+  key: string;
+  value: any;
+};
