5c58f4895dbf12546de01790b038ef3ffed28db5
Varun Taneja <varunt298@gmail.com>
2026-01-12 13:27:23 +0530

    sdk flag polling

--- sdk-js/src/client.ts
+++ sdk-js/src/client.ts
@@ -6,22 +6,27 @@ type ClientOptions = {
   apiUrl: string;
   environment: "dev" | "prod";
   user: UserContext;
+  pollIntervalMs?: number;
 };
 
 export class FeatureFlagClient {
   private apiUrl: string;
   private environment: "dev" | "prod";
   private user: UserContext;
+  private pollInterval: number;
+  private usedFlags = new Set<string>();
 
   constructor(options: ClientOptions) {
     this.apiUrl = options.apiUrl;
     this.environment = options.environment;
     this.user = options.user;
+    this.pollInterval = options.pollIntervalMs ?? 15000;
+
+    this.startPolling();
   }
 
   async getFlag<T = any>(key: string): Promise<T> {
     const cacheKey = `${key}:${this.environment}:${this.user.userId}`;
-    console.log(`cacheKey: ${cacheKey}`);
 
     const cached = getCache(cacheKey);
     if (cached !== undefined) {
@@ -38,6 +43,23 @@ export class FeatureFlagClient {
     );
 
     setCache(cacheKey, response.value);
+    this.usedFlags.add(key);
+
     return response.value;
+  }
+
+  private startPolling() {
+    setInterval(async () => {
+      for (const key of this.usedFlags) {
+        const response = await evaluateFlagRequest(this.apiUrl, {
+          key,
+          environment: this.environment,
+          context: this.user
+        });
+
+        const cacheKey = `${key}:${this.environment}:${this.user.userId}`;
+        setCache(cacheKey, response.value);
+      }
+    }, this.pollInterval);
   }
 }
--- sdk-js/src/test.ts
+++ sdk-js/src/test.ts
@@ -1,41 +1,13 @@
 import { FeatureFlagClient } from "./index.js";
 
-// const client = new FeatureFlagClient({
-//   apiUrl: "http://localhost:8080/api",
-//   environment: "prod",
-//   user: {
-//     userId: "user_1123",
-//     region: "US",
-//     subscriptionType: "premium"
-//   }
-// });
-
-// const run = async () => {
-//   const checkoutEnabled = await client.getFlag<boolean>("checkout_v3");
-//   console.log("checkout_v3:", checkoutEnabled);
-
-//   const analytics = await client.getFlag<boolean>("advanced_analytics");
-//   console.log("advanced_analytics:", analytics);
-
-//   for (let i = 0; i < 5; i++) {
-//   const value = await client.getFlag<boolean>("checkout_v3");
-//   console.log(i, value);
-//   }
-// };
-
-// run();
-
-const clientA = new FeatureFlagClient({
+const sdk = new FeatureFlagClient({
   apiUrl: "http://localhost:8080/api",
   environment: "prod",
-  user: { userId: "user_1", region: "US" }
+  user: { userId: "user_1", region: "US" },
+  pollIntervalMs: 5000
 });
 
-const clientB = new FeatureFlagClient({
-  apiUrl: "http://localhost:8080/api",
-  environment: "prod",
-  user: { userId: "user_2" }
-});
-
-console.log(await clientA.getFlag("checkout_v3"));
-console.log(await clientB.getFlag("checkout_v3"));
+setInterval(async () => {
+  const value = await sdk.getFlag<boolean>("checkout_v3");
+  console.log(new Date().toISOString(), value);
+}, 3000);
